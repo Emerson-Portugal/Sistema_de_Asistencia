50e981a802f1cc1f3aa111df42c2a69a21ee90bd
Emerson-Portugal <emersoncrp@gmail.com>
2024-01-19 21:38:14 -0500

    Implementacion

--- backend/app/db/connection.py
+++ backend/app/db/connection.py
@@ -5,8 +5,10 @@ def create_connection():
     dbname = 'test_asistencia'
     user = 'postgres'
     password = '7895'
+    #password = 'admin7895'
     host = 'localhost'
     port = '5432'
+    #port = '8086'
 
     try:
         conn = psycopg2.connect(
